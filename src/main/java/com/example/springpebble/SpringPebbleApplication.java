5849b62ee7921baad4b93bd62aa5f5855c2158d7
yookeun <knoc720@gmail.com>
2018-01-10 22:43:45 +0900

    first commit

---
+++ src/main/java/com/example/springpebble/SpringPebbleApplication.java
@@ -0,0 +1,12 @@
+package com.example.springpebble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPebbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPebbleApplication.class, args);
+	}
+}
